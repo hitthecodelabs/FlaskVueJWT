fdea2f034f2eaf6aaae71c72da7d0f8ed27a57d1
Hit the Code Labs <145279880+hitthecodelabs@users.noreply.github.com>
2024-05-19 16:08:19 -0500

    Update main.js

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,21 +1,18 @@
 // src/main.js
 
-import { createApp, reactive } from 'vue';
+import { createApp } from 'vue';
 import VueToast from 'vue-toast-notification';
 import App from './App.vue';
 import router from './router';
+import { createPinia } from 'pinia';
 import './assets/styles.css';  // global styles
 import 'vue-toast-notification/dist/theme-default.css';  // CSS for the toast
 
 const app = createApp(App);
 
 // Define a global reactive state
-const globalState = reactive({
-  isLoggedIn: false,
-
-});
-
-app.provide('globalState', globalState);
+const pinia = createPinia();
+app.use(pinia);
 
 app.use(VueToast, {
   position: 'top-right',
